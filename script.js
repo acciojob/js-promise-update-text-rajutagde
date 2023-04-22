8190a36f1821581dc64b3b12b565038b6af7f0a2
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-22 13:07:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function updateOutput() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+updateOutput().then((message) => {
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerHTML = message;
+});
